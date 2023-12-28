5c49e0dbe9a1f82dc690164314f702c4e42c815f
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-12-28 14:39:05 +0100

    Update product model with clearer description and price with currency

--- server/src/resources/product/product.model.js
+++ server/src/resources/product/product.model.js
@@ -3,8 +3,19 @@ const Joi = require("joi");
 
 const ProductSchema = new Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
+  description: [
+    {
+      text: { type: String, required: true },
+      specifications: { type: [String], required: false },
+      careAdvice: { type: String, required: false },
+    },
+  ],
+  price: [
+    {
+      unit_amount: { type: Number, required: true },
+      currency: { type: String, required: true },
+    },
+  ],
   image: { type: String, required: true },
   inStock: { type: Number, required: true, default: 0 },
   categories: {
@@ -19,11 +30,26 @@ const ProductModel = models.product || model("product", ProductSchema);
 
 const ProductCreateValidationSchema = Joi.object({
   title: Joi.string().strict().required(),
-  description: Joi.string().strict().required(),
-  price: Joi.number().strict().required(),
+  price: Joi.array()
+    .items(
+      Joi.object({
+        unit_amount: Joi.number().strict().required(),
+        currency: Joi.string().strict().required(),
+      })
+    )
+    .required(),
   image: Joi.string().uri().allow("image/png", "image/jpeg").required(),
   inStock: Joi.number().strict().required(),
   categories: Joi.array().min(1),
+  description: Joi.array()
+    .items(
+      Joi.object({
+        text: Joi.string().strict().required(),
+        specifications: Joi.array().items(Joi.string().strict()),
+        cleaning: Joi.string().strict(),
+      })
+    )
+    .required(),
 });
 
 const ProductUpdateValidationSchema = ProductCreateValidationSchema.keys({
