2a5ecffb91f2e6f46e8e5af838849a9dca9a8716
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-12-29 10:20:57 +0100

    Create adminOnly and auth in middlewares.js

--- server/src/resources/middlewares.js
+++ server/src/resources/middlewares.js
@@ -1,3 +1,15 @@
+/** Prevent access to users without admin privilege */
+function adminOnly(req, res, next) {
+  if (req.session?.isAdmin) return next();
+  res.status(403).json("You don not have permissions to perform this request");
+}
+
+/** Prevent access to logged out users */
+function auth(req, res, next) {
+  if (req.session?._id) return next();
+  res.status(401).json("You must login to perform this request");
+}
+
 /** Validate request body based on a joi schema */
 function validate(joiSchema) {
   return (req, res, next) => {
@@ -17,6 +29,8 @@ function exists(Model) {
 }
 
 module.exports = {
+  adminOnly,
+  auth,
   exists,
   validate,
 };
