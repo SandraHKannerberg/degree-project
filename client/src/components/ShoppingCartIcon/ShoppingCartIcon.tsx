47470cd32d6e11441f0b522620ee180571a8303b
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-01-09 15:45:33 +0100

    Create ShoppingCartIcon component, im port in header

---
+++ client/src/components/ShoppingCartIcon/ShoppingCartIcon.tsx
@@ -0,0 +1,18 @@
+import { Cart } from "react-bootstrap-icons";
+
+function ShoppingCartIcon() {
+  return (
+    <>
+      <Cart
+        style={{
+          border: "none",
+          borderRadius: 0,
+          color: "#EFE1D1",
+          fontSize: "2.5rem",
+        }}
+      ></Cart>
+    </>
+  );
+}
+
+export default ShoppingCartIcon;
