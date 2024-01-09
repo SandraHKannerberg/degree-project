54397f963dc07aaecba5cad203e608dc618b1d31
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-01-09 14:58:20 +0100

    Change from Container wrapper to fragment wrapper

--- client/src/components/Logotype/Logotype.tsx
+++ client/src/components/Logotype/Logotype.tsx
@@ -1,15 +1,14 @@
 import logotype from "../../assets/logotype.png";
-import { Container } from "react-bootstrap";
 
 function Logotype() {
   return (
-    <Container>
+    <>
       <img
         src={logotype}
         alt="Lotus Harmony logotype"
         style={{ maxHeight: "4rem" }}
       />
-    </Container>
+    </>
   );
 }
 
