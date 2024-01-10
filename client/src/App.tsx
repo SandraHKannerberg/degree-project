86b0e9789c0f3933427c02d02a16bc9c6b678218
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-01-10 15:10:18 +0100

    Create ProductContext and add ProductProvider in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Confirmation from "./pages/Confirmation";
 import Home from "./pages/Home/Home";
+import Shop from "./pages/Shop/Shop";
+import { ProductProvider } from "./context/ProductContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/confirmation" element={<Confirmation />} />
-      </Routes>
+      <ProductProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+        </Routes>
+      </ProductProvider>
     </BrowserRouter>
   );
 }
---
+++ client/src/context/ProductContext.tsx
@@ -0,0 +1,90 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  PropsWithChildren,
+  useEffect,
+  Dispatch,
+  SetStateAction,
+} from "react";
+
+//Exsisting product
+export interface Product {
+  _id: string;
+  title: string;
+  brand?: string;
+  description: string;
+  careAdvice?: string;
+  features?: string[];
+  price: number;
+  image: string;
+  inStock: number;
+  categories?: string[];
+  stripeProductId: string;
+}
+
+//New product
+export interface NewProduct {
+  title: string;
+  brand?: string;
+  description: string;
+  careAdvice?: string;
+  features?: string[];
+  price: number;
+  image: string;
+  inStock: number;
+  categories: string[];
+}
+
+export interface IProductContext {
+  products: Product[];
+  setProducts: Dispatch<SetStateAction<Product[]>>;
+  getAllProducts: () => void;
+}
+
+const defaultValues = {
+  products: [],
+  setProducts: () => {},
+  getAllProducts: () => {},
+};
+
+export const ProductContext = createContext<IProductContext>(defaultValues);
+
+export const useProductContext = () => useContext(ProductContext);
+
+export const ProductProvider = ({ children }: PropsWithChildren<{}>) => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  //Get all products
+  const getAllProducts = async () => {
+    try {
+      const responseFetchProducts = await fetch("api/products");
+
+      // Check response status
+      if (!responseFetchProducts.ok) {
+        throw new Error("Failed to fetch products");
+      }
+
+      const productData = await responseFetchProducts.json();
+      setProducts(productData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //   useEffect(() => {
+  //     getAllProducts();
+  //   }, []);
+
+  return (
+    <ProductContext.Provider
+      value={{
+        products,
+        setProducts,
+        getAllProducts,
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+};
