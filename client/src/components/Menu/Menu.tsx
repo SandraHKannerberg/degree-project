0917724b7298bb805ca1d4014c795aec9c334a6d
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-01-11 14:49:55 +0100

    Update menu with responsive layout, other colors and hover effect

--- client/src/components/Menu/Menu.tsx
+++ client/src/components/Menu/Menu.tsx
@@ -1,6 +1,8 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
 import { useProductContext } from "../../context/ProductContext";
 import { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import "./Menu.css";
 
 //Menu with categories and link to each category
 function Menu() {
@@ -11,19 +13,49 @@ function Menu() {
   }, []);
 
   return (
-    <Navbar>
-      <Container className="d-flex justify-content-center">
-        <Nav>
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/shop">All</Nav.Link>
-          {categories.map((category, index) => (
-            <Nav.Link key={index} href={`/${category._id}`}>
-              {category.title}
-            </Nav.Link>
-          ))}
-        </Nav>
-      </Container>
-    </Navbar>
+    <>
+      {["md"].map((expand) => (
+        <Navbar
+          expand={expand}
+          className="mb-3"
+          style={{ backgroundColor: "#DFD3C3" }}
+        >
+          <Container fluid>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="start"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Menu
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body className="d-flex justify-content-center">
+                <Nav className="gap-md-3 gap-lg-4 gap-xl-5">
+                  <NavLink to="/" className="menu-link">
+                    Home
+                  </NavLink>
+                  <NavLink to="/shop" className="menu-link">
+                    All
+                  </NavLink>
+                  {categories.map((category, index) => (
+                    <NavLink
+                      key={index}
+                      to={`/categories/${category._id}`}
+                      className="menu-link"
+                    >
+                      {category.title}
+                    </NavLink>
+                  ))}
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
   );
 }
 
