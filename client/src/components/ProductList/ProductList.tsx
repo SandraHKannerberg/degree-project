3307fdf89d001359713783e26c563402c3b68381
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-01-11 19:36:23 +0100

    Create ProductCard as an individual  component

--- client/src/components/ProductList/ProductList.tsx
+++ client/src/components/ProductList/ProductList.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 import { useProductContext } from "../../context/ProductContext";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import ProductCard from "../ProductCard/ProductCard";
 
+// Show all products in the shop
 function ProductList() {
   const { products, getAllProducts } = useProductContext();
 
@@ -27,42 +29,7 @@ function ProductList() {
       >
         {products.map((product, index) => (
           <Col key={index} className="mb-3">
-            <Card className="h-100">
-              <Card.Img variant="top" src={product.image} />
-              <Card.Body className="d-flex flex-column">
-                <Card.Title
-                  className="flex-grow-1"
-                  style={{ fontSize: "18px" }}
-                >
-                  {product.title}
-                </Card.Title>
-                <Card.Text className="flex-grow-1 mb-0">
-                  {product.brand}
-                </Card.Text>
-                <Card.Text className="d-flex justify-content-end">
-                  {product.price} SEK
-                </Card.Text>
-                <Row className="justify-content-between">
-                  <Col xs={6}>
-                    <Card.Text>{product.inStock} inStock</Card.Text>
-                  </Col>
-                  <Col xs={6} className="d-flex justify-content-end">
-                    <Button
-                      size="sm"
-                      style={{
-                        backgroundColor: "#85586f",
-                        border: "none",
-                        borderRadius: 0,
-                        color: "#EFE1D1",
-                        fontWeight: 500,
-                      }}
-                    >
-                      Add to cart
-                    </Button>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
+            <ProductCard product={product} key={product._id} />
           </Col>
         ))}
       </Row>
