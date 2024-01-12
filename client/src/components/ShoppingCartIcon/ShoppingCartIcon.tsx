a3eb739094d9147d0df03fff9acc8be58e1112b4
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-01-12 14:44:26 +0100

    Add badge to shoppingCartIcon

--- client/src/components/ShoppingCartIcon/ShoppingCartIcon.tsx
+++ client/src/components/ShoppingCartIcon/ShoppingCartIcon.tsx
@@ -1,8 +1,9 @@
+import { Badge, Col } from "react-bootstrap";
 import { Cart } from "react-bootstrap-icons";
 
 function ShoppingCartIcon() {
   return (
-    <>
+    <Col lg={4} className="position-relative">
       <Cart
         style={{
           border: "none",
@@ -11,7 +12,14 @@ function ShoppingCartIcon() {
           fontSize: "2.5rem",
         }}
       ></Cart>
-    </>
+      <Badge
+        bg="dark"
+        className="position-absolute top-0 end-0"
+        style={{ borderRadius: "50%" }}
+      >
+        9
+      </Badge>
+    </Col>
   );
 }
 
