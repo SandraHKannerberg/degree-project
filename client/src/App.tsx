19376f06c9c9dda70516bc53ceb12f5dceb7edc5
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-01-16 15:32:39 +0100

    Create CartContext with all needed content and add the CartProvider to App.js

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProductProvider } from "./context/ProductContext";
+import { CartProvider } from "./context/CartContext";
 import Home from "./pages/Home/Home";
 import Shop from "./pages/Shop/Shop";
 import About from "./pages/About/About";
@@ -11,17 +12,19 @@ import ProductsCategory from "./pages/Shop/ProductsCategory";
 function App() {
   return (
     <BrowserRouter>
-      <ProductProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/confirmation" element={<Confirmation />} />
-          <Route path="/categories/:id" element={<ProductsCategory />} />
-          <Route path="/:id" element={<ProductDetails />} />
-        </Routes>
-      </ProductProvider>
+      <CartProvider>
+        <ProductProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/confirmation" element={<Confirmation />} />
+            <Route path="/categories/:id" element={<ProductsCategory />} />
+            <Route path="/:id" element={<ProductDetails />} />
+          </Routes>
+        </ProductProvider>
+      </CartProvider>
     </BrowserRouter>
   );
 }
---
+++ client/src/context/CartContext.tsx
@@ -0,0 +1,122 @@
+import {
+  createContext,
+  useContext,
+  PropsWithChildren,
+  Dispatch,
+  SetStateAction,
+} from "react";
+
+import useLocalStorage from "../hooks/useLocalStorage";
+
+export interface CartItem {
+  id: string; //Id from stripePriceId
+  quantity: number;
+}
+
+export interface ICartContext {
+  cartItems: CartItem[];
+  setCartItems: Dispatch<SetStateAction<CartItem[]>>;
+  addToCart: (id: string) => void;
+  getCartItemQuantity: (id: string) => void;
+  decreaseCartQuantity: (id: string) => void;
+  removeFromCart: (id: string) => void;
+  emptyCart: () => void;
+  cartTotalQuantity: number;
+}
+
+const defaultValues = {
+  cartItems: [],
+  setCartItems: () => {},
+  addToCart: () => "",
+  getCartItemQuantity: () => {},
+  decreaseCartQuantity: () => {},
+  removeFromCart: () => {},
+  emptyCart: () => {},
+  cartTotalQuantity: 0,
+};
+
+export const CartContext = createContext<ICartContext>(defaultValues);
+
+export const useCartContext = () => useContext(CartContext);
+
+export const CartProvider = ({ children }: PropsWithChildren<{}>) => {
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
+    "shopping-cart",
+    []
+  );
+
+  // Count total items in cart
+  const cartTotalQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
+
+  // This function handle the quantity of every cartItem in the shoppingcart
+  function getCartItemQuantity(id: string) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  }
+
+  // Function to add a product to shoppingcart or increase the quantity if the product already in cart
+  function addToCart(id: string) {
+    console.log("addToCart function called with ID:", id);
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id) == null) {
+        return [...currItems, { id, quantity: 1 }];
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  // Function to be able to decrease cartitem quantity
+  function decreaseCartQuantity(id: string) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id)?.quantity === 1) {
+        return currItems.filter((item) => item.id !== id);
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  // Remove item from cart
+  function removeFromCart(id: string) {
+    setCartItems((currItems) => {
+      return currItems.filter((item) => item.id !== id);
+    });
+  }
+
+  // This functions empty the cart
+  function emptyCart() {
+    setCartItems([]);
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        setCartItems,
+        addToCart,
+        getCartItemQuantity,
+        cartTotalQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+        emptyCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
