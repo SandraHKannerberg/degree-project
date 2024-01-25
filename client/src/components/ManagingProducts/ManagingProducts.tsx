222b733c7a408b011065009a735efe4efdcdadde
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-01-25 20:01:45 +0100

    DeleteProduct - done

--- client/src/components/ManagingProducts/ManagingProducts.tsx
+++ client/src/components/ManagingProducts/ManagingProducts.tsx
@@ -1,9 +1,10 @@
 import { useUserContext } from "../../context/UserContext";
 import { useProductContext } from "../../context/ProductContext";
-import { useEffect } from "react";
-import { Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Container, Row, Col, Modal, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Trash, Pen } from "react-bootstrap-icons";
+import AddProductForm from "../AddProductForm/AddProductForm";
 
 // Component for managing products
 // You need to be logged in with admin auth to be able to access this productlist
@@ -11,69 +12,149 @@ import { Trash, Pen } from "react-bootstrap-icons";
 function ManagingProducts() {
   const { loggedInUser } = useUserContext();
 
-  const { products, getAllProducts } = useProductContext();
+  const {
+    products,
+    getAllProducts,
+    // updateProductInDatabase,
+    deleteProductFromDatabase,
+    // title,
+    // setTitle,
+    // image,
+    // setImage,
+    // brand,
+    // setBrand,
+    // description,
+    // setDescription,
+    // price,
+    // setPrice,
+    // inStock,
+    // setInStock,
+    // careAdvice,
+    // setCareAdvice,
+    // features,
+    // setFeatures,
+    // categories,
+    // setCategories,
+  } = useProductContext();
+
+  // States for modal in the context of delete
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [showComplete, setShowComplete] = useState(false);
+
+  // Handle the first modal that requires confirm before delete
+  const handleCloseConfirm = () => setShowConfirm(false);
+  const handleShowConfirm = () => setShowConfirm(true);
+
+  // Handle the second modal to show confirm after delete
+  const handleCloseComplete = () => setShowComplete(false);
+  const handleShowComplete = () => setShowComplete(true);
 
   useEffect(() => {
     getAllProducts();
   }, []);
 
+  //Eventlistener on delete button
+  const handleDelete = async (
+    event: React.MouseEvent<HTMLElement>,
+    id: string
+  ) => {
+    event.preventDefault();
+    deleteProductFromDatabase(id);
+
+    // Close the first modal and show the next one to confirm the delete
+    handleCloseConfirm();
+    handleShowComplete();
+  };
+
   return (
     <>
       {loggedInUser?.isAdmin && (
-        <>
-          {/* Wrapped container around productlist */}
-          <Container fluid className="mx-1">
-            <h1>Products</h1>
-            <Link to={"/admin"} style={{ padding: 0 }}>
-              Go back
-            </Link>
+        <Container fluid className="mx-1">
+          <h1>Products</h1>
+          <Link to={"/admin"} style={{ padding: 0 }}>
+            Go back
+          </Link>
 
-            <table className="table table-striped align-middle shadow-sm rounded">
-              <thead>
-                <tr>
-                  <th>Image</th>
-                  <th>Title</th>
-                  <th>Brand</th>
-                  <th>Description</th>
-                  <th>Price</th>
-                  <th>InStock</th>
-                  <th>CareAdvice</th>
-                  <th>Features</th>
-                  <th>Category</th>
-                  <th>Edit</th>
-                  <th>Delete</th>
-                </tr>
-              </thead>
-              <tbody style={{ fontSize: "14px" }}>
-                {products.map((product, index) => (
-                  <tr key={index}>
-                    <td>
-                      <img
-                        src={product.image}
-                        alt={product.title}
-                        style={{ maxWidth: "50px", maxHeight: "50px" }}
-                      />
-                    </td>
-                    <td>{product.title}</td>
-                    <td>{product.brand}</td>
-                    <td>{product.description}</td>
-                    <td>{product.price} SEK</td>
-                    <td>{product.inStock}</td>
-                    <td>{product.careAdvice}</td>
-                    <td>{product.features}</td>
-                    <td>{product.categories}</td>
-                    <td>
-                      <Pen></Pen>
-                    </td>
-                    <td>
-                      <Trash></Trash>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </Container>
-        </>
+          <Row>
+            <AddProductForm></AddProductForm>
+          </Row>
+
+          <Row>
+            {products.map((product, index) => (
+              <Row key={index} className="mb-3">
+                <Col>
+                  {" "}
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                    style={{ maxWidth: "50px", maxHeight: "50px" }}
+                  />
+                </Col>
+                <Col>{product.title}</Col>
+                <Col>{product.brand}</Col>
+                <Col>{product.description}</Col>
+                <Col>{product.price} SEK</Col>
+                <Col>{product.inStock}</Col>
+                <Col>{product.careAdvice}</Col>
+                <Col>{product.features}</Col>
+                <Col>{product.categories}</Col>
+                <Col>
+                  <Button variant="dark">
+                    <Pen></Pen>
+                  </Button>
+                  <Button variant="danger" onClick={handleShowConfirm}>
+                    <Trash></Trash>
+                  </Button>
+                </Col>
+
+                {/* Modal to require confirm before delete */}
+                <Modal
+                  show={showConfirm}
+                  onHide={handleCloseConfirm}
+                  backdrop="static"
+                  keyboard={false}
+                >
+                  <Modal.Header closeButton>
+                    <Modal.Title>Confirm delete</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    Are you sure you want to delete this product? The action is
+                    permanent and can't be undone.
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseConfirm}>
+                      Close
+                    </Button>
+                    <Button
+                      variant="danger"
+                      onClick={(e) => handleDelete(e, product._id)}
+                    >
+                      Yes, delete
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+
+                {/* Modal to confirm that the delete was successfull */}
+                <Modal
+                  show={showComplete}
+                  onHide={handleCloseComplete}
+                  backdrop="static"
+                  keyboard={false}
+                >
+                  <Modal.Header closeButton>
+                    <Modal.Title>Deleted</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>The product are now deleted</Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseComplete}>
+                      OK
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+              </Row>
+            ))}
+          </Row>
+        </Container>
       )}
     </>
   );
