9485bbb1121d8f8a8e1e161a0c1fe0165e2b6ff9
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-01-28 08:06:20 +0100

    Add lazyLoading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,35 @@
+import React from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProductProvider } from "./context/ProductContext";
 import { CartProvider } from "./context/CartContext";
 import { OrderProvider } from "./context/OrderContext";
-import Home from "./pages/Home/Home";
-import Shop from "./pages/Shop/Shop";
-import About from "./pages/About/About";
-import ContactUs from "./pages/ContactUs/ContactUs";
-import Confirmation from "./pages/Confirmation/Confirmation";
-import ProductDetails from "./pages/Shop/ProductDetails";
-import ProductsCategory from "./pages/Shop/ProductsCategory";
-import UserProvider from "./context/UserContext";
-import User from "./pages/User/User";
-import Admin from "./pages/Admin/Admin";
-import Orders from "./components/Orders/Orders";
-import ManagingOrders from "./components/ManagingOrders/ManagingOrders";
-import ManagingProducts from "./components/ManagingProducts/ManagingProducts";
-import AddProductForm from "./components/AddProductForm/AddProductForm";
+import { UserProvider } from "./context/UserContext";
+
+//LazyLoaing
+const Home = React.lazy(() => import("./pages/Home/Home"));
+const Shop = React.lazy(() => import("./pages/Shop/Shop"));
+const About = React.lazy(() => import("./pages/About/About"));
+const ContactUs = React.lazy(() => import("./pages/ContactUs/ContactUs"));
+const Confirmation = React.lazy(
+  () => import("./pages/Confirmation/Confirmation")
+);
+const ProductDetails = React.lazy(() => import("./pages/Shop/ProductDetails"));
+const ProductsCategory = React.lazy(
+  () => import("./pages/Shop/ProductsCategory")
+);
+const User = React.lazy(() => import("./pages/User/User"));
+const Admin = React.lazy(() => import("./pages/Admin/Admin"));
+const Orders = React.lazy(() => import("./components/Orders/Orders"));
+const ManagingOrders = React.lazy(
+  () => import("./components/ManagingOrders/ManagingOrders")
+);
+const ManagingProducts = React.lazy(
+  () => import("./components/ManagingProducts/ManagingProducts")
+);
+const AddProductForm = React.lazy(
+  () => import("./components/AddProductForm/AddProductForm")
+);
 
 function App() {
   return (
@@ -25,19 +39,111 @@ function App() {
           <ProductProvider>
             <OrderProvider>
               <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/shop" element={<Shop />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<ContactUs />} />
-                <Route path="/confirmation" element={<Confirmation />} />
-                <Route path="/categories/:id" element={<ProductsCategory />} />
-                <Route path="/product/:id" element={<ProductDetails />} />
-                <Route path="/mypage" element={<User />} />
-                <Route path="/mypage/orders" element={<Orders />} />
-                <Route path="/admin" element={<Admin />} />
-                <Route path="/admin/orders" element={<ManagingOrders />} />
-                <Route path="/admin/products" element={<ManagingProducts />} />
-                <Route path="/admin/addproduct" element={<AddProductForm />} />
+                <Route
+                  path="/"
+                  element={
+                    <React.Suspense fallback={<div>Loading...</div>}>
+                      <Home />
+                    </React.Suspense>
+                  }
+                />
+                <Route
+                  path="/shop"
+                  element={
+                    <React.Suspense fallback={<div>Loading...</div>}>
+                      <Shop />
+                    </React.Suspense>
+                  }
+                />
+                <Route
+                  path="/about"
+                  element={
+                    <React.Suspense fallback={<div>Loading...</div>}>
+                      <About />
+                    </React.Suspense>
+                  }
+                />
+                <Route
+                  path="/contact"
+                  element={
+                    <React.Suspense fallback={<div>Loading...</div>}>
+                      <ContactUs />
+                    </React.Suspense>
+                  }
+                />
+                <Route
+                  path="/confirmation"
+                  element={
+                    <React.Suspense fallback={<div>Loading...</div>}>
+                      <Confirmation />
+                    </React.Suspense>
+                  }
+                />
+                <Route
+                  path="/categories/:id"
+                  element={
+                    <React.Suspense fallback={<div>Loading...</div>}>
+                      <ProductsCategory />
+                    </React.Suspense>
+                  }
+                />
+                <Route
+                  path="/product/:id"
+                  element={
+                    <React.Suspense fallback={<div>Loading...</div>}>
+                      <ProductDetails />
+                    </React.Suspense>
+                  }
+                />
+                <Route
+                  path="/mypage"
+                  element={
+                    <React.Suspense fallback={<div>Loading...</div>}>
+                      <User />
+                    </React.Suspense>
+                  }
+                />
+                <Route
+                  path="/mypage/orders"
+                  element={
+                    <React.Suspense fallback={<div>Loading...</div>}>
+                      <Orders />
+                    </React.Suspense>
+                  }
+                />
+                <Route
+                  path="/admin"
+                  element={
+                    <React.Suspense fallback={<div>Loading...</div>}>
+                      <Admin />
+                    </React.Suspense>
+                  }
+                />
+                <Route
+                  path="/admin/orders"
+                  element={
+                    <React.Suspense fallback={<div>Loading...</div>}>
+                      <ManagingOrders />
+                    </React.Suspense>
+                  }
+                />
+                <Route
+                  path="/admin/products"
+                  element={
+                    <React.Suspense fallback={<div>Loading...</div>}>
+                      <ManagingProducts />
+                    </React.Suspense>
+                  }
+                />
+
+                <Route
+                  path="/admin/addproduct"
+                  element={
+                    <React.Suspense fallback={<div>Loading...</div>}>
+                      <AddProductForm />
+                    </React.Suspense>
+                  }
+                />
               </Routes>
             </OrderProvider>
           </ProductProvider>
