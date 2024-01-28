564b1cba1a3a4387d8b33232d80400db00bf590a
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-01-28 21:20:49 +0100

    Update responsive css - banner

--- client/src/components/Banner/Banner.tsx
+++ client/src/components/Banner/Banner.tsx
@@ -9,68 +9,75 @@ import LazyLoad from "react-lazy-load";
 function Banner() {
   return (
     <Row
+      className="d-flex flex-column justify-content-center align-items-center"
       style={{
         backgroundImage: `url(${border})`,
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
       }}
     >
-      <h1
-        className="mt-3"
-        style={{ textAlign: "center", fontFamily: "Julius Sans One" }}
-      >
-        Lotus Harmony
-      </h1>
-      <Row>
+      <Col xs={12} md={12} style={{ marginTop: "4rem" }}>
+        <h1
+          className="mt-3"
+          style={{ textAlign: "center", fontFamily: "Julius Sans One" }}
+        >
+          Lotus Harmony
+        </h1>
         <p style={{ textAlign: "center", fontStyle: "italic" }}>
           Find Your Zen, Embrace the Harmony - Lotus Harmony, Where Your Yoga
           Journey Begins
         </p>
-      </Row>
+      </Col>
 
-      <Col className="d-flex justify-content-center gap-5 my-4">
-        <LazyLoad height={300} width={300}>
-          <img
-            className="shadow"
-            src={yoga1}
-            alt="Yoga"
-            style={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-              objectPosition: "0 95%",
-              borderRadius: "50%",
-            }}
-          />
-        </LazyLoad>
-        <LazyLoad height={300} width={300}>
-          <img
-            className="shadow"
-            src={yoga2}
-            alt="Yoga"
-            style={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-              objectPosition: "0 90%",
-              borderRadius: "50%",
-            }}
-          />
-        </LazyLoad>
-        <LazyLoad height={300} width={300}>
-          <img
-            className="shadow"
-            src={yoga3}
-            alt="Yoga"
-            style={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-              objectPosition: "0 70%",
-              borderRadius: "50%",
-            }}
-          />
-        </LazyLoad>
+      <Col
+        xs={12}
+        md={12}
+        className="d-flex flex-md-column justify-content-center align-items-center"
+      >
+        <Row className="d-flex justify-content-center gap-5 my-4">
+          <LazyLoad height={300} width={300}>
+            <img
+              className="shadow"
+              src={yoga1}
+              alt="Yoga"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                objectPosition: "0 95%",
+                borderRadius: "50%",
+              }}
+            />
+          </LazyLoad>
+          <LazyLoad height={300} width={300}>
+            <img
+              className="shadow"
+              src={yoga2}
+              alt="Yoga"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                objectPosition: "0 90%",
+                borderRadius: "50%",
+              }}
+            />
+          </LazyLoad>
+          <LazyLoad height={300} width={300}>
+            <img
+              className="shadow"
+              src={yoga3}
+              alt="Yoga"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                objectPosition: "0 70%",
+                borderRadius: "50%",
+              }}
+            />
+          </LazyLoad>
+        </Row>
       </Col>
     </Row>
   );
