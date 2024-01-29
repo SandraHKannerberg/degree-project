8018d4b3f925143bdf9fe9e9e5da2a5e155d67cd
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-01-29 16:42:54 +0100

    Add Pagination to ProductList

--- client/src/components/ProductList/ProductList.tsx
+++ client/src/components/ProductList/ProductList.tsx
@@ -1,16 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useProductContext } from "../../context/ProductContext";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Pagination, Row } from "react-bootstrap";
 import ProductCard from "../ProductCard/ProductCard";
 import Banner from "../Banner/Banner";
 
 // Show all products in the shop
 function ProductList() {
   const { products, getAllProducts } = useProductContext();
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 12; //Products per page
 
   useEffect(() => {
     getAllProducts();
   }, []);
+
+  // Count index for first and last product on current page
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  // Change page
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return (
     <Container fluid style={{ padding: 0, marginTop: "7.5rem" }}>
@@ -22,12 +35,29 @@ function ProductList() {
         lg={4}
         className="d-flex justify-content-center px-4 gy-4 mt-2 flex-wrap"
       >
-        {products.map((product, index) => (
+        {currentProducts.map((product, index) => (
           <Col key={index} className="mb-3">
             <ProductCard product={product} key={product._id} />
           </Col>
         ))}
       </Row>
+
+      {/* Pagination */}
+      <Pagination className="justify-content-center">
+        {Array.from(
+          { length: Math.ceil(products.length / productsPerPage) },
+          (_, index) => (
+            <Pagination.Item
+              key={index}
+              active={index + 1 === currentPage}
+              onClick={() => paginate(index + 1)}
+              className="customize-pagination"
+            >
+              {index + 1}
+            </Pagination.Item>
+          )
+        )}
+      </Pagination>
     </Container>
   );
 }
