9beeb9c1acc9ef9e9865b5f7c48ac56fdfea02ad
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-01-31 10:00:56 +0100

    Search function in ProductList component

--- client/src/components/ProductList/ProductList.tsx
+++ client/src/components/ProductList/ProductList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { useProductContext } from "../../context/ProductContext";
+import { Product, useProductContext } from "../../context/ProductContext";
 import { Col, Container, Pagination, Row } from "react-bootstrap";
+import { Search } from "react-bootstrap-icons";
 import ProductCard from "../ProductCard/ProductCard";
 import Banner from "../Banner/Banner";
 
@@ -8,16 +9,30 @@ import Banner from "../Banner/Banner";
 function ProductList() {
   const { products, getAllProducts } = useProductContext();
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
   const productsPerPage = 12; //Products per page
 
   useEffect(() => {
     getAllProducts();
   }, []);
 
+  // Search function
+  useEffect(() => {
+    if (!searchTerm) {
+      setFilteredProducts(products);
+    } else {
+      const filtered = products.filter((product) =>
+        product.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredProducts(filtered);
+    }
+  }, [searchTerm, products]);
+
   // Count index for first and last product on current page
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = products.slice(
+  const currentProducts = filteredProducts.slice(
     indexOfFirstProduct,
     indexOfLastProduct
   );
@@ -28,6 +43,36 @@ function ProductList() {
   return (
     <Container fluid style={{ padding: 0, marginTop: "7.5rem" }}>
       <Banner></Banner>
+      {/* Search */}
+      <Row
+        style={{ marginTop: "2rem" }}
+        className="d-flex justify-content-center"
+      >
+        <Col
+          xs={10}
+          sm={8}
+          md={5}
+          lg={3}
+          className="d-flex justify-content-center mx-4"
+        >
+          <span className="input-group-text">
+            <Search></Search>
+          </span>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder=" Search products..."
+            className="p-2 w-100"
+            style={{
+              border: "2px solid lightgrey",
+              color: "#3F2E3E",
+              backgroundColor: "#fff",
+            }}
+          />
+        </Col>
+      </Row>
+
       <Row
         xs={1}
         sm={2}
@@ -45,7 +90,7 @@ function ProductList() {
       {/* Pagination */}
       <Pagination className="justify-content-center">
         {Array.from(
-          { length: Math.ceil(products.length / productsPerPage) },
+          { length: Math.ceil(filteredProducts.length / productsPerPage) },
           (_, index) => (
             <Pagination.Item
               key={index}
