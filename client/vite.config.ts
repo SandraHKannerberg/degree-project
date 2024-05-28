35205cb488cdfe5bba05832b494a5ff8f175c8de
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-05-28 22:31:28 +0200

    Debugging fetch problems

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        // target: "http://localhost:3000",
-        target: "https://degree-project.onrender.com",
-      },
-    },
-  },
-  plugins: [react()],
-});
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       "/api": {
+//         // target: "http://localhost:3000",
+//         target: "https://degree-project.onrender.com",
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// });
