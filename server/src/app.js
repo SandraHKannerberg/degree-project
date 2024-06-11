1c2c5c7754757d31d541245aa2b85825558ae2a2
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-06-11 14:50:11 +0200

    Update cookie settings

--- server/src/app.js
+++ server/src/app.js
@@ -34,7 +34,7 @@ app.use(
     name: "session",
     keys: [cookieSecretKey],
     maxAge: 1000 * 60 * 60 * 24, // 24 Hours
-    sameSite: "none",
+    sameSite: "Strict",
     httpOnly: true,
     secure: false,
   })
