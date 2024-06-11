d5c490034e74036bb3b78659c0667ad6bc65fc9f
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-06-11 14:54:19 +0200

    .

--- server/src/app.js
+++ server/src/app.js
@@ -34,7 +34,7 @@ app.use(
     name: "session",
     keys: [cookieSecretKey],
     maxAge: 1000 * 60 * 60 * 24, // 24 Hours
-    sameSite: "Strict",
+    sameSite: "None",
     httpOnly: true,
     secure: false,
   })
