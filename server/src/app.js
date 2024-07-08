47a4a1e5e6c10b232bd02813398367c4408b2024
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-07-08 19:54:03 +0200

    .

--- server/src/app.js
+++ server/src/app.js
@@ -39,7 +39,7 @@ app.use(
     maxAge: 1000 * 60 * 60 * 24, // 24 Hours
     sameSite: "none",
     httpOnly: false,
-    // secure: true,
+    secure: true,
     path: '/',
     domain: [DOMAIN],
   })
