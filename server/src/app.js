ab7adf0e6831e47787dd6a0c80bbf5baef794388
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-07-10 23:09:57 +0200

    Update secure-setting for cookie

--- server/src/app.js
+++ server/src/app.js
@@ -41,7 +41,7 @@ app.use(
     maxAge: 1000 * 60 * 60 * 24, // 24 Hours
     sameSite: "none",
     httpOnly: true,
-    secure: false,
+    secure: true,
     path: '/',
     domain: [DOMAIN],
   })
