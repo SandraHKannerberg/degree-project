682080395f09f5f8b08fc31e77979214901ae4d8
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2024-07-12 00:38:12 +0200

    Done with debugging cookis, cors and verify payment - problems solved

--- server/src/resources/checkout/checkout.controller.js
+++ server/src/resources/checkout/checkout.controller.js
@@ -116,8 +116,6 @@ const verifySession = async (req, res) => {
       req.body.sessionId
     );
 
-    console.log('LINE-ITEMS', line_items);
-
     // Create new order
     const newOrder = new OrderModel({
       customer: session.customer_details.name,
@@ -154,7 +152,6 @@ const verifySession = async (req, res) => {
       stripePaymentIntentId: session.payment_intent,
     });
 
-    console.log("ORDER: ", newOrder);
     // Save order in MongoDB
     await newOrder.save();
 
@@ -171,16 +168,6 @@ const verifySession = async (req, res) => {
     console.log(response);
     
     res.status(200).json(response);
-
-    // res.status(200).json({
-    //   verified: true,
-    //   message: "New order successfully created",
-    //   orderDetails: {
-    //     email: newOrder.email,
-    //     totalAmount: newOrder.totalAmount,
-    //     orderNumber: newOrder.orderNumber,
-    //   },
-    // });
   } catch (error) {
     console.error(error.message);
     res.status(500).json({ verified: false, error: "Internal Server Error" });
