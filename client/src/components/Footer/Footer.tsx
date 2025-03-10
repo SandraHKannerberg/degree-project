ae669a624fe823e51983046625b2d6e67ae3cfa4
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-03-10 13:14:19 +0100

    Improvements of code structure, accessibility and semantic html for header and footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -6,98 +6,99 @@ import { Link } from "react-router-dom";
 // Subscribe-section and socialmedia-icons are just for show
 function Footer() {
   return (
-    <Container
-      fluid
-      style={{
-        background:
-          "linear-gradient(0deg, rgba(223,211,195,1) 0%, rgba(239,225,209,1) 100%)",
-        minHeight: "30vh",
-        marginBottom: 0,
-      }}
-      className="w-100 shadow"
-    >
-      <Row className="text-center mt-2 pt-3">
-        <Col>
-          <h1
-            style={{
-              color: "#331d2c",
-              fontFamily: "Julius Sans One",
-              fontSize: "30px",
-            }}
-          >
-            Lotus Harmony
-          </h1>
-        </Col>
-      </Row>
-      <Row className="d-flex flex-column justify-content-center align-items-center">
-        <Col xs={6} className="text-center pb-3">
-          <p style={{ fontSize: "14px", padding: 0, margin: 0 }}>
-            Subscribe to receive our offers in preview and enjoy 10% off on your
-            first order
-          </p>
-        </Col>
-        <Col xs={10} sm={8} md={7} lg={5} className="pb-3">
-          <InputGroup className="mb-3">
-            <Form.Control
-              placeholder="Enter your e-mail"
-              aria-label="E-mail"
-              aria-describedby="basic-addon2"
+    <footer className="w-100 shadow bg-light-clr-gradient py-3">
+      <Container>
+        {/* Company Name */}
+        <Row className="text-center mt-2 pt-3">
+          <Col>
+            <h3
               style={{
-                backgroundColor: "#f8ede3",
-                border: "1px solid #331d2c",
-              }}
-            />
-            <InputGroup.Text
-              id="basic-addon2"
-              style={{
-                backgroundColor: "#331d2c",
-                color: "#EFE1D1",
-                border: "1px solid #331d2c",
+                color: "#331d2c",
+                fontFamily: "Julius Sans One",
+                fontSize: "30px",
               }}
             >
-              Subscribe
-            </InputGroup.Text>
-          </InputGroup>
-        </Col>
-      </Row>
-      <Row className="d-flex justify-content-around text-center">
-        <Col>
-          <h6>
-            <Link to="/about" className="menu-link">
-              About us
-            </Link>
-          </h6>
-        </Col>
-        <Col>
-          <h6>
-            <Link to="/contact" className="menu-link">
-              Contact us
-            </Link>
-          </h6>
-        </Col>
-        <Col className="d-flex justify-content-center text-center">
-          <div className="d-flex gap-2">
-            <h5>
-              <Facebook />
-            </h5>
-            <h5>
-              <Instagram />
-            </h5>
-            <h5>
-              <Linkedin />
-            </h5>
-          </div>
-        </Col>
-        <Col
-          xs={12}
-          className="d-flex justify-content-center alig-items-center pt-3"
-        >
-          <p style={{ fontSize: "12px" }}>
-            Copyright &copy; Sandra Höst Kannerberg - Degree project 2024, Medieinstitutet
-          </p>
-        </Col>
-      </Row>
-    </Container>
+              Lotus Harmony
+            </h3>
+          </Col>
+        </Row>
+
+        {/* Newsletter Subscription */}
+        <Row className="d-flex flex-column justify-content-center align-items-center mt-4">
+          <Col xs={12} className="text-center pb-3 fs-6">
+            <p>
+              Subscribe to receive our offers in preview and enjoy 10% off on
+              your first order
+            </p>
+          </Col>
+          <Col xs={10} sm={8} md={7} lg={5} className="pb-3">
+            <InputGroup className="mb-3">
+              {/* Input */}
+              <Form.Control
+                type="email"
+                placeholder="Enter your e-mail"
+                aria-label="E-mail"
+                aria-describedby="e-mail"
+                required
+              />
+              {/* Button */}
+              <InputGroup.Text id="e-mail" className="bg-dark text-white">
+                Subscribe
+              </InputGroup.Text>
+            </InputGroup>
+          </Col>
+        </Row>
+
+        {/* Links */}
+        <Row className="d-flex justify-content-around text-center mb-3">
+          <Col>
+            <h4>
+              <Link
+                to="/about"
+                className="menu-link fs-5 text-dark text-decoration-none"
+              >
+                About us
+              </Link>
+            </h4>
+          </Col>
+          <Col>
+            <h4>
+              <Link
+                to="/contact"
+                className="menu-link fs-5 text-dark text-decoration-none"
+              >
+                Contact us
+              </Link>
+            </h4>
+          </Col>
+        </Row>
+
+        {/* Social Media Icons */}
+        <Row className="d-flex justify-content-center text-center mb-3">
+          <Col className="d-flex justify-content-center gap-3">
+            <a href="#" aria-label="Facebook" className="text-dark">
+              <Facebook size={32} />
+            </a>
+            <a href="#" aria-label="Instagram" className="text-dark">
+              <Instagram size={32} />
+            </a>
+            <a href="#" aria-label="LinkedIn" className="text-dark">
+              <Linkedin size={32} />
+            </a>
+          </Col>
+        </Row>
+
+        {/* Copyright */}
+        <Row className="d-flex justify-content-center text-center pt-3">
+          <Col>
+            <small>
+              Copyright &copy; Sandra Höst Kannerberg - Degree project 2024,
+              Medieinstitutet
+            </small>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 }
 
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,49 +1,39 @@
 import LogInOffcanvas from "../LogInOffcanvas/LogInOffcanvas";
 import Logotype from "../Logotype/Logotype";
 import ShoppingCart from "../ShoppingCart/ShoppingCart";
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Menu from "../Menu/Menu";
+import { Container } from "react-bootstrap";
 
 // Header component with Logotype, User-icon (for login or signup), Shoppingcart and menu of categories
 function Header() {
   return (
-    <Container
-      fluid
-      style={{
-        backgroundColor: "#A78295",
-        height: "5rem",
-      }}
-      className="p-0 mb-5 fixed-top"
-    >
-      <Row
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          height: "100%",
-          paddingLeft: "2rem",
-          paddingRight: "2rem",
-          margin: 0,
-        }}
-      >
-        <Col xs={6} style={{ padding: 0 }}>
-          <Logotype />
-        </Col>
-        <Col
-          xs={6}
-          className="d-flex justify-content-end align-items-center"
-          style={{ padding: 0 }}
-        >
-          <div className="d-flex gap-2 align-items-center">
+    <header className="fixed-top w-100">
+      {/* Main header container */}
+      <Container fluid className="px-4 bg-accent-clr">
+        <Row className="m-0 align-items-center">
+          {/* Logotype */}
+          <Col xs={6} className="p-0">
+            <a href="/" aria-label="Homepage">
+              <Logotype />
+            </a>
+          </Col>
+
+          {/* Icons (Login and Cart) */}
+          <Col
+            xs={6}
+            className="d-flex justify-content-end align-items-center p-0"
+          >
             <LogInOffcanvas />
             <ShoppingCart />
-          </div>
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* Main Menu */}
       <Menu />
-    </Container>
+    </header>
   );
 }
 
