64a7d3fea6be69502ef1cde609b2dc984acd3793
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-03-19 21:51:24 +0100

    Create a new layout for homepage

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,37 +1,49 @@
 import Header from "../../components/Header/Header";
-import LogotypeLightColor from "../../components/Logotype/LogotypeLightColor";
 import yoga from "../../assets/yoga.mp4";
 import { Container, Row } from "react-bootstrap";
 import "./Home.css";
-import Footer from "../../components/Footer/Footer";
-import ShopByCategory from "../../components/ShopByCategory/ShopByCategory";
-import BannerInspiration from "../../components/Banner/BannerInspiration";
-import BannerSubscribe from "../../components/Banner/BannerSubscribe";
-import BannerSignup from "../../components/Banner/BannerSignup";
+import { Link } from "react-router-dom";
 
 //Landingpage
 function Home() {
   return (
     <>
       <Header />
-      <Container fluid className="page-wrapper">
-        <Row className="d-flex flex-column justify-content-center align-items-center text-center">
-          <LogotypeLightColor />
-          <h1 className="text-center title-font">Lotus Harmony</h1>
-
-          <p className="text-center fst-italic mb-5">
-            Find Your Zen, Embrace the Harmony - Lotus Harmony, Where Your Yoga
-            Journey Begins
-          </p>
-        </Row>
-        <Row className="video-background d-flex justify-content-center">
-          <video className="video" autoPlay loop muted>
+      <Container fluid className="p-0 m-0">
+        <Row className="video-background d-flex justify-content-center align-items-center p-0 m-0">
+          <video
+            className="video"
+            autoPlay
+            loop
+            muted
+            playsInline
+            preload="auto"
+            style={{
+              position: "absolute",
+              top: "0",
+              left: "0",
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+            }}
+          >
             <source src={yoga} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
+
+          <section className="hero-content text-center text-white position-relative">
+            <h1 className="display-4 title-font text-light-clr ">
+              Lotus Harmony
+            </h1>
+            <Link
+              to={"/shop"}
+              className="lead text-decoration-none text-light-clr"
+            >
+              Start shopping
+            </Link>
+          </section>
         </Row>
       </Container>
-      <Footer />
     </>
   );
 }
